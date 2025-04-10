fcc3d5707106e587a5d8609fcc786f9be70f2daf
mdraihan51674 <mdraihan51674@gmail.com>
2025-04-10 11:09:13 +0600

    line add

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -15,7 +15,7 @@ const Banner = () => {
       {/* Overlay with text */}
       <div className="relative z-10 w-full h-full flex flex-col items-start justify-center text-white px-10">
         <h1 className="text-4xl font-bold mb-4 text-left">
-          Bid on Unique Items from Around the World
+          Bid on Unique Items from  <br /> Around the World
         </h1>
         <p className="text-lg mb-6 text-left max-w-xl">
           Discover rare collectibles, luxury goods, and vintage treasures in our curated auctions.
